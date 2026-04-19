634519367415ebad4b9556e8df1afee3980a0f62
tkdlrs <tkd.lsanchez@gmail.com>
2026-04-19 14:58:52 -0600

    Reviews the solution files for 'Chapter 1: Setup | Lesson 3: Normalize URLs' of the 'Build a Web Scraper in Python' course on Boot.dev.

--- crawl.py
+++ crawl.py
@@ -1,16 +1,8 @@
-from urllib.parse import urlsplit
+from urllib.parse import urlparse
 
-def normalize_url(input_url):
-    url_object = urlsplit(input_url)
-    # 
-    scheme, netloc, path, query, fragment = url_object
-    # 
-    output = f"{netloc}{path}"
-    if output[-1] == "/":
-        output = output[0:-1]
-    # 
-    return output 
-
-
-if __name__ == "__main__":
-    normalize_url()
+def normalize_url(url):
+    parsed_url = urlparse(url)
+    full_path = f"{parsed_url.netloc}{parsed_url.path}"
+    full_path = full_path.rstrip("/") 
+    return full_path.lower()
+# 
--- test_crawl.py
+++ test_crawl.py
@@ -13,7 +13,6 @@ class TestCrawl(unittest.TestCase):
         actual = normalize_url(input_url)
         expected = "www.boot.dev/blog/path"
         self.assertEqual(actual, expected)
-
     #
     def test_normalize_url_2(self):
         # 
@@ -27,7 +26,31 @@ class TestCrawl(unittest.TestCase):
             a = normalize_url(case.i)
             self.assertEqual(a, case.e)      
     # 
+    def test_normalize_url_protocol(self):
+        input_url = "https://crawler-test.com/path"
+        actual = normalize_url(input_url)
+        expected = "crawler-test.com/path"
+        self.assertEqual(actual, expected)
+    # 
+    def test_normalize_url_slash(self):
+        input_url = "https://crawler-test.com/path/"
+        actual = normalize_url(input_url)
+        expected = "crawler-test.com/path"
+        self.assertEqual(actual, expected)
+    # 
+    def test_normalize_url_capitals(self):
+        input_url = "https://CRAWLER-TEST.com/path"
+        actual = normalize_url(input_url)
+        expected = "crawler-test.com/path"
+        self.assertEqual(actual, expected)
+    # 
+    def test_normalize_url_http(self):
+        input_url = "http://CRAWLER-TEST.com/path"
+        actual = normalize_url(input_url)
+        expected = "crawler-test.com/path"
+        self.assertEqual(actual, expected)
+    #     
+    
 # 
 if __name__ == "__main__":
     unittest.main()
-
