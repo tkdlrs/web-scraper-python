6bc20e3c5e9f2a009af0a0b808521e59780247f6
tkdlrs <tkd.lsanchez@gmail.com>
2026-04-22 20:25:34 -0600

    Completes 'Chapter 2: Crawling | Lesson 1: Main' of the 'Build a Web Scraper in Python' guided project on Boot.dev.

--- main.py
+++ main.py
@@ -1,5 +1,19 @@
+import sys
+# 
 def main():
-    print("Hello from web-scraper-python!")
+    sysArgs = sys.argv
+    # 
+    if len(sysArgs) < 2:
+        print("no website provided")
+        sys.exit(1)
+    # 
+    if len(sysArgs) > 2:
+        print("too many arguments provided")
+        sys.exit(1)
+    # 
+    BASE_URL = sysArgs[1]
+    print(f"starting crawl of: {BASE_URL}")
+    sys.exit(0)
 
 
 if __name__ == "__main__":
